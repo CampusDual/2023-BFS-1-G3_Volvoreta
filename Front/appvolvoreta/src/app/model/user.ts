5f8829c1632f97cc26ab5bf0845f7771aeb187ee
Carlos Fontán <74905493+fgjcarlos@users.noreply.github.com>
2023-05-29 14:29:42 +0200

    Delete directory Front, change framework Ontimize

--- Front/appvolvoreta/src/app/model/user.ts
+++
@@ -1,9 +0,0 @@
-export class User{
-    id       : number;
-    nif      : string;
-    name     : string;
-    surname1 : string;
-    sunarme2 : string;
-    login    : string;
-    password : string;
-}
--- Front/appvolvoreta/src/app/model/userRoles.ts
+++
@@ -1,5 +0,0 @@
-export class UserRoles{
-    id       : number;
-    userId  : number;
-    roleId  : number;
-}
