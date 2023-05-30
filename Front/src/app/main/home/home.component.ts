16618dc691caf40ec31e247aac970f13641ed0cf
CeciaVarelaRodriguez <ceciavarelar@gmail.com>
2023-05-30 14:31:48 +0200

    Recoger usuario Front

--- Front/src/app/main/home/home.component.ts
+++ Front/src/app/main/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService, OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'home',
@@ -7,11 +8,30 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  user: any;
+  nombre: any;
 
   constructor(
     private router: Router,
-    private actRoute: ActivatedRoute
+    private actRoute: ActivatedRoute,
+    @Inject(AuthService) private authService: AuthService,
+    public injector: Injector,
+    private ontimizeService: OntimizeService
   ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('users'));
+    this.ontimizeService.query(undefined, ['USER_', 'NAME', 'SURNAME1', 'ROLENAME'], 'user').subscribe(
+      res => {
+        console.log(res.data)
+        if (res.data && res.data.length) {
+          this.user = res.data.filter((e: any) => e['USER_'] === this.authService.getSessionInfo().user)[0];
+        }else{
+          this.user = undefined;
+        }
+        console.log(this.user)
+      },
+      err => console.log(err)
+
+    );
   }
 
   ngOnInit() {
