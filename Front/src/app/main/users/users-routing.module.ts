afc7aec56083abd2d6971355a83c43b42ae80763
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-06-01 14:42:26 +0200

    primera parte generar contraseñas

--- Front/src/app/main/users/users-routing.module.ts
+++ Front/src/app/main/users/users-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersHomeComponent } from './users-home/users-home.component';
+import { UsersDetailComponent } from './users-detail/users-detail.component';
 
 
 const routes: Routes =  [{
   path : '',
   component: UsersHomeComponent
+},
+{
+  path: ":USER_",
+  component: UsersDetailComponent
 }];
 
 @NgModule({
--- Front/src/app/main/users/users.module.ts
+++ Front/src/app/main/users/users.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { UsersHomeComponent } from './users-home/users-home.component';
+import { UsersDetailComponent } from './users-detail/users-detail.component';
 
 
 @NgModule({
-  declarations: [UsersHomeComponent],
+  declarations: [UsersHomeComponent, UsersDetailComponent],
   imports: [
     CommonModule,
     UsersRoutingModule,
