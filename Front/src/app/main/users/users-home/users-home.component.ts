ca4ed64a4f605ee512a6e7602da4ebecb020522f
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-06-02 12:59:30 +0200

    Update users-home.component.ts

--- Front/src/app/main/users/users-home/users-home.component.ts
+++ Front/src/app/main/users/users-home/users-home.component.ts
@@ -29,7 +29,7 @@ export class UsersHomeComponent implements OnInit {
 
     }
     static show(){
-      console.log('Algo');
+      console.log('hola');
     }
     show(){
       UsersHomeComponent.show();
