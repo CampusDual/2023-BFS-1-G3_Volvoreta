053dc5313377fd0255bd0847c1ffc6a7d333579b
CeciaVarelaRodriguez <ceciavarelar@gmail.com>
2023-06-12 14:47:52 +0200

    Product Card con imagen
    Product Card con imagen

--- Front/src/app/main/products/products-cards/products-cards.component.ts
+++ Front/src/app/main/products/products-cards/products-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products-cards',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsCardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngAfterContentChecked() {
+    this.cd.detectChanges();
+  }
 
   ngOnInit() {
   }
