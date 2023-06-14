f411258fd15fa7b2b31851f2037ee731349b92ff
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-06-14 09:56:20 +0200

    slide toogle
    select, radio, translate

--- Front/src/app/main/products/products-new/products-new.component.ts
+++ Front/src/app/main/products/products-new/products-new.component.ts
@@ -8,20 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsNewComponent implements OnInit {
 
   constructor() { }
-  getDataArray() {
-    const array: Array<Object> = [];
-    array.push({
-      'key': 0,
-      'value': 'false'
-    });
-    array.push({
-      'key': 1,
-      'value': 'true'
-    });
-    
-    return array;
-  }
-
+  
   ngOnInit() {
   }
 
