f751263a9c43041cfd499ad99d6762509177b7bf
Carlos Fontan <fgjcarlos@gmail.com>
2023-06-21 11:53:38 +0200

    Añadimos control de errores formulario insertar nuevo producto

--- Front/src/app/main/products/products-new/products-new.component.ts
+++ Front/src/app/main/products/products-new/products-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { OComboComponent } from 'ontimize-web-ngx';
+import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { OComboComponent, ODateInputComponent, OValidators } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-products-new',
@@ -7,15 +8,75 @@ import { OComboComponent } from 'ontimize-web-ngx';
   styleUrls: ['./products-new.component.css']
 })
 export class ProductsNewComponent implements OnInit {
-  @ViewChild("stateC", {static: true}) selectC: OComboComponent;
+  @ViewChild("stateC", { static: true }) selectC: OComboComponent;
+  @ViewChild("inputDate", { static: true }) inputDate: ODateInputComponent;
 
-  constructor() { }
-  
-  ngOnInit() {
+  validatorsArray: ValidatorFn[] = [];
+  today: Date = new Date()
+  private VALIDATOR = {
+    HASCHAR: "* Debes introducir un número entero y positivo",
+    HASNEGATIVE: "* Debes introducir un número positivo"
   }
- defaultId(event){
-    // if(event.length == 0){
-      this.selectC.setValue(2);
-    // }
- }
+
+  constructor() {
+    this.validatorsArray.push(OValidators.patternValidator(/[1-9]/, 'hasNumber'));
+    this.validatorsArray.push(this.stockValidator)
+    this.validatorsArray.push(this.priceValidator)
+  }
+
+  ngOnInit() {
+
+    console.log(this.today.getDate())
+
+  }
+
+  defaultId(event) {
+    this.selectC.setValue(2);
+  }
+
+  defaultDate() {
+
+    let today: Date = new Date()
+    console.log("event")
+    console.log(today.getDate())
+
+    this.inputDate.setData(today.getDate())
+
+    this.inputDate.setValue(today.getDate());
+  }
+
+  // Funcion custom para personalizar la validacion
+  stockValidator(control: FormControl): ValidationErrors {
+
+    if (!control.value) return
+
+    let itemsNotIncludes = ['.', ',', 'e', '-']
+
+    let auxArr: string[] = control.value.toString().split("")
+
+    for (let item = 0; item < auxArr.length; item++) {
+
+      if (itemsNotIncludes.includes(auxArr[item])) {
+        return { 'hasChar': true, };
+      }
+
+    }
+  }
+
+  // Funcion custom para personalizar la validacion
+  priceValidator(control: FormControl): ValidationErrors {
+
+    if (!control.value) return
+
+    console.log(control.value.toString().includes('-'))
+
+    if (control.value.toString().includes('-')) {
+      return { 'hasNegative': true, };
+    }
+
+    return {}
+
+  }
+
+
 }
