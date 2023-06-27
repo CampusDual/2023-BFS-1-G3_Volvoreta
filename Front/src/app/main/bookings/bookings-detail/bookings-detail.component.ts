20776eafa94b61b48a81d1b7c6fd95b2677e8f80
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-06-27 18:07:22 +0200

    Cancelado, confirmado, entregado, fecha de entrega, tarea crono

--- Front/src/app/main/bookings/bookings-detail/bookings-detail.component.ts
+++ Front/src/app/main/bookings/bookings-detail/bookings-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { OComboComponent } from 'ontimize-web-ngx';
+import { OComboComponent, ODateInputComponent, OFormComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-bookings-detail',
@@ -8,29 +8,52 @@ import { OComboComponent } from 'ontimize-web-ngx';
 })
 export class BookingsDetailComponent implements OnInit {
   @ViewChild("rState", { static: true }) rState: OComboComponent;
+  @ViewChild("detailBooking", { static: true }) detailBooking: OFormComponent;
+  //@ViewChild("picked", { static: true }) picked: ODateInputComponent;
 
   enabled: boolean;
+  
 
   constructor() {
     this.enabled = false;
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
+  
   enable(event){
     const { type } = event
     
-    // console.log(event)
+    console.log(event)
     // console.log(event.type)
     // console.log(newValue)
     // console.log(oldValue)
     // console.log(this.rState.getValue())
     // console.log(this.rState.getSelectedItems())
     // console.log(this.rState.getSelectedRecord())
+
+    if(Number(this.rState.getValue()) == 2 && type == 1){
+      this.enabled = false;
+      document.getElementById("picked").setAttribute("style", "display:none");
+    } else {
+      if(Number(this.rState.getValue()) == 1 || type == 0){
+        this.enabled = true;
+        document.getElementById("picked").setAttribute("style", "display:none");
+      } else {
+        document.getElementById("iState").setAttribute("style", "display:none");
+      }
+      
+    }
     
-    this.enabled = (Number(this.rState.getValue()) == 2 && type == 1) ? false : true;
+    // this.enabled = (Number(this.rState.getValue()) == 2 && type == 1) ? false : true;
 
   }
-
+  // hidden(event){
+  //   console.log(this.detailBooking.getDataValue('collection_completed'))
+  //   console.log(event)
+  //   const { newValue } = event
+  //   if(newValue.value == undefined){
+  //     document.getElementById("picked").setAttribute("style", "display:none");
+  //   }
+    
+  // }
 }
