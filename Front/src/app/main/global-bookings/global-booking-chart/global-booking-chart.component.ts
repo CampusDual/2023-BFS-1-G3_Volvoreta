5dd31eceb19ae061ddb91adc0c6e0feb39a36fd2
Carlos Fontan <fgjcarlos@gmail.com>
2023-06-30 15:03:09 +0200

    Cambios en el chart de stats

--- Front/src/app/main/global-bookings/global-booking-chart/global-booking-chart.component.ts
+++ Front/src/app/main/global-bookings/global-booking-chart/global-booking-chart.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { DataAdapterUtils, DiscreteBarChartConfiguration, OChartComponent } from 'ontimize-web-ngx-charts';
-import { D3LocaleService} from 'src/app/shared/d3-locale/d3Locale.service';
+import { D3LocaleService } from 'src/app/shared/d3-locale/d3Locale.service';
 import { ReserveService } from 'src/app/services/reserve.service';
+import { Reserve } from 'src/app/models/reserve';
 
 @Component({
   selector: 'app-global-booking-chart',
@@ -9,8 +10,10 @@ import { ReserveService } from 'src/app/services/reserve.service';
   styleUrls: ['./global-booking-chart.component.css']
 })
 export class GlobalBookingChartComponent implements OnInit {
-  @ViewChild('discretebar',{static:true}) protected discretebar: OChartComponent;
+  @ViewChild('discretebar', { static: true }) protected discretebar: OChartComponent;
   public chartParameters: DiscreteBarChartConfiguration;
+
+  dataStats: any;
 
   noTranslateData = [
     { "date": 1672574400000, "value": 1 },
@@ -26,24 +29,43 @@ export class GlobalBookingChartComponent implements OnInit {
     { "date": 1698840000000, "value": 1 },
     { "date": 1701432000000, "value": 5 }
   ];
-    
-  constructor(private reserveService: ReserveService, @Inject(D3LocaleService) private d3LocaleService: D3LocaleService) {
 
-   }
+  constructor(private reserveService: ReserveService, @Inject(D3LocaleService) private d3LocaleService: D3LocaleService) {
+  }
 
   ngOnInit() {
+
+    console.log(new Date(1685620800000))
+
+    this.reserveService.getReserveDate().subscribe(({ data }) => {
+      this.dataStats = data
+
+      console.log(new Date(data[0]["collection_completed"]))
+
+      this.configCharData()
+    });
+
+  }
+
+  private sumUnitByMonth() {
+
+  }
+
+  private configCharData() {
+    console.log(this.dataStats)
     const d3Locale = this.d3LocaleService.getD3LocaleConfiguration();
     this._configureDiscreteBarChart(d3Locale);
     let dataAdapter = DataAdapterUtils.createDataAdapter(this.chartParameters);
-    this.discretebar.setDataArray(dataAdapter.adaptResult(this.noTranslateData));
-    this.reserveService.getReserveDate().subscribe(res => console.log(res.data));
+    this.discretebar.setDataArray(dataAdapter.adaptResult(this.dataStats));
   }
+
   private _configureDiscreteBarChart(locale: any): void {
-    let colors:string[] = ['#1464a5','#4649A6','#41bf78','#363636','#006bdb']
+
+    let colors: string[] = ['#1464a5', '#4649A6', '#41bf78', '#363636', '#006bdb']
     this.chartParameters = new DiscreteBarChartConfiguration();
-    this.chartParameters.xAxis = "date";
-    this.chartParameters.yAxis = ["value"];
+    this.chartParameters.xAxis = "collection_completed";
+    this.chartParameters.yAxis = ["units"];
     this.chartParameters.color = colors;
-    this.chartParameters.xDataType = d => locale.timeFormat('%b')(new Date(d));
-  }
+    this.chartParameters.xDataType = d => locale.timeFormat('%b')(new Date(d))
+  };
 }
