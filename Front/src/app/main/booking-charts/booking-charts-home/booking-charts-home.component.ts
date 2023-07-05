facd528f3bdfa4398d0bc565d34a5b9e7dd5a28b
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-07-05 09:48:53 +0200

    Traducción automática en msg bienvenida

--- Front/src/app/main/booking-charts/booking-charts-home/booking-charts-home.component.ts
+++ Front/src/app/main/booking-charts/booking-charts-home/booking-charts-home.component.ts
@@ -10,10 +10,13 @@ import { FnTranslator } from 'src/app/utils/fnTranslator';
   styleUrls: ['./booking-charts-home.component.css']
 })
 export class BookingChartsHomeComponent implements OnInit {
-  @ViewChild('discretebar',{static:true}) protected discretebar: OChartComponent;
+  @ViewChild('discretebar1',{static:true}) protected discretebar1: OChartComponent;
+  @ViewChild('discretebar2',{static:true}) protected discretebar2: OChartComponent;
   
   public chartParameters: DiscreteBarChartConfiguration;
-  protected graphData: Array<Object>;
+  public chartParameters2: DiscreteBarChartConfiguration;
+  protected graphDataS: Array<Object>;
+  protected graphDataP: Array<Object>;
   // protected criteriaValue = 50;
   
   constructor(private ontimizeService: OntimizeService, 
@@ -21,12 +24,18 @@ export class BookingChartsHomeComponent implements OnInit {
             private d3LocaleService: D3LocaleService, 
             public injector: Injector) { 
 
-    this.graphData = [];
+    this.graphDataS = [];
+    this.graphDataP = [];
+    this.getSalles();
+    this.getProfits();
+  }
+
+  getSalles(){
     this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('bookings'));
     this.ontimizeService.query({"reservation_state": 3, "year_": 2023}, ['total_sales','month_','year_'], 'sellBooking').subscribe(
       res => {
         if (res && res.data.length && res.code === 0) {
-          this.adaptResult(res.data);
+          this.adaptResult(res.data, this.graphDataS, 1);
         }
       },
       err => console.log(err),
@@ -40,23 +49,44 @@ export class BookingChartsHomeComponent implements OnInit {
     // this.chartParameters.x1Axis.showMaxMin = false;
     this.chartParameters.xAxis = "key";
     this.chartParameters.yAxis = ["values"];
-    this.chartParameters.color = ['#1464a5','#4649A6','#41bf78','#363636','#006bdb'];
-    
+    //this.chartParameters.color = ['#1464a5','#4649A6','#41bf78','#363636','#006bdb'];
+  }
+  getProfits(){
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('bookings'));
+    this.ontimizeService.query({"reservation_state": 3, "year_": 2023}, ['profits','month_','year_'], 'sellBooking').subscribe(
+      res => {
+        if (res && res.data.length && res.code === 0) {
+          this.adaptResult(res.data, this.graphDataP, 2);
+        }
+      },
+      err => console.log(err),
+      () => this.cd.detectChanges()
+    );
+    this.chartParameters2 = new DiscreteBarChartConfiguration();
+    this.chartParameters2.xAxis = "key";
+    this.chartParameters2.yAxis = ["values"];
+    this.chartParameters2.color = ['#1464a5','#4649A6','#41bf78','#363636','#006bdb'];
   }
 
-  adaptResult(data: any) {
+  adaptResult(data: any, graphData: any[], numero: number) {
+    graphData = [];
     if (data && data.length) {
-      let values = this.processValues(data);
+      let values = this.processValues(data, numero);
       let keys = this.processKeys(data);
       // chart data
       keys.forEach((item: any, items: number) => {
         // values.forEach(() => {
           const linea: object[] = [{'key': item, 'values': values[items]}]; console.log(linea)
-          this.graphData.push(linea[0]); console.log(this.graphData);
+          graphData.push(linea[0]); console.log(graphData);
         // });
       });
       let dataAdapter = DataAdapterUtils.createDataAdapter(this.chartParameters);
-      this.discretebar.setDataArray(dataAdapter.adaptResult(this.graphData));
+      if(numero === 1){
+        this.discretebar1.setDataArray(dataAdapter.adaptResult(graphData));
+      } else if(numero === 2){
+        this.discretebar2.setDataArray(dataAdapter.adaptResult(graphData));
+      }
+     
     }
   }
   processKeys(data: any) {
@@ -107,11 +137,16 @@ export class BookingChartsHomeComponent implements OnInit {
     });
     return keys;
   }
-  processValues(data: any) {
+  processValues(data: any, numero: number) {
     let values = [];
-    
+    let valor = "";
+    if(numero === 1){
+      valor = 'total_sales';
+    } else if(numero === 2){
+      valor = 'profits';
+    }
     data.forEach((item: any, index: number) => {
-      values.push(item['total_sales']);
+      values.push(item[valor]);
     });
     return values;
   }
--- Front/src/app/utils/fnTranslator.ts
+++ Front/src/app/utils/fnTranslator.ts
@@ -6,29 +6,29 @@ export class FnTranslator {
 
     }
 
-    translateRolename(user: User): string {
-        // console.log(localStorage)
-        if (JSON.parse(localStorage.getItem("com.ontimize.web.volvoreta"))['lang'] == "es") {
-            let role: string;
-            if (user.rolename == 'security') {
-                role = 'seguridad';
-            } else if (user.rolename == 'maintenance') {
-                role = 'mantenimiento';
-            } else if (user.rolename == 'user'){
-                role = 'usuario';
-            } else {
-                role = 'no autorizado';
-            }
-            return "Bienvenid@, " + user.name + " " + user.surname1 + ";  acceso: " + role;
-        } else {
-            if(user.rolename == '' || user.rolename == null){
-                return "Welcome, " + user.name+ " " + user.surname1 + "; access type: not authorized";
-            } else {
-                return "Welcome, " + user.name+ " " + user.surname1 + "; access type: " + user.rolename;
-            }
+    // translateRolename(user: User): string {
+    //     // console.log(localStorage)
+    //     if (JSON.parse(localStorage.getItem("com.ontimize.web.volvoreta"))['lang'] == "es") {
+    //         let role: string;
+    //         if (user.rolename == 'security') {
+    //             role = 'seguridad';
+    //         } else if (user.rolename == 'maintenance') {
+    //             role = 'mantenimiento';
+    //         } else if (user.rolename == 'user'){
+    //             role = 'usuario';
+    //         } else {
+    //             role = 'no autorizado';
+    //         }
+    //         return "Bienvenid@, " + user.name + " " + user.surname1 + ";  acceso: " + role;
+    //     } else {
+    //         if(user.rolename == '' || user.rolename == null){
+    //             return "Welcome, " + user.name+ " " + user.surname1 + "; access type: not authorized";
+    //         } else {
+    //             return "Welcome, " + user.name+ " " + user.surname1 + "; access type: " + user.rolename;
+    //         }
             
-        }
-    }
+    //     }
+    // }
 
     translateMonth(month_: number): string {
         if (JSON.parse(localStorage.getItem("com.ontimize.web.volvoreta"))['lang'] == "es") {
