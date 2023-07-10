faffa93c817bf13e6fed52310f909567e16ea7f0
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-07-11 00:16:01 +0200

    permisos y gráficas users

--- Front/src/app/main/booking-charts/booking-charts-users/booking-charts-users.component.ts
+++ Front/src/app/main/booking-charts/booking-charts-users/booking-charts-users.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild, Injector } from '@angular/core';
 import { OntimizeService } from 'ontimize-web-ngx';
-import { DataAdapterUtils, DiscreteBarChartConfiguration, OChartComponent } from 'ontimize-web-ngx-charts';
+import { DiscreteBarChartConfiguration, OChartComponent } from 'ontimize-web-ngx-charts';
 
 @Component({
   selector: 'app-booking-charts-users',
@@ -8,67 +8,118 @@ import { DataAdapterUtils, DiscreteBarChartConfiguration, OChartComponent } from
   styleUrls: ['./booking-charts-users.component.css']
 })
 export class BookingChartsUsersComponent implements OnInit {
+  @ViewChild('discretebar1',{static:true}) protected discretebar1: OChartComponent;
+  @ViewChild('discretebar2',{static:true}) protected discretebar2: OChartComponent;
 
-  @ViewChild('discretebar',{static:true}) protected discretebar: OChartComponent;
-  
+  total_not_picked_up: any;
   public chartParameters: DiscreteBarChartConfiguration;
+  public chartParameters2: DiscreteBarChartConfiguration;
   protected graphData: Array<Object>;
-  // protected criteriaValue = 50;
-  
-  constructor(private ontimizeService: OntimizeService, 
-            private cd: ChangeDetectorRef, 
-            public injector: Injector) { 
+  protected graphDataU: Array<Object>;
+  private auxGraph: Array<Object>;
+  protected criteriaValue = 2;
 
+
+  constructor(
+    private ontimizeService: OntimizeService,
+    private cd: ChangeDetectorRef, 
+    public injector: Injector
+  ) {
     this.graphData = [];
-    this.getSalles();
+    this.graphDataU = [];
+    this.auxGraph = [];
+    this.getCount();
   }
-
-  getSalles(){
+  getCount(){
     this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('bookings'));
-    this.ontimizeService.query({"reservation_state": 4, "year_": 2023}, ['id_user','name','year_', 'surname1', 'not_picked_up'], 'usersBooking').subscribe(
+    this.ontimizeService.query({"reservation_state": 4, "year_": 2023}, ['id_user','name', 'surname1', 'not_picked_up','year_'], 'usersBooking').subscribe(
       res => {
         if (res && res.data.length && res.code === 0) {
-          this.adaptResult(res.data);
+          this.total_not_picked_up = res.data.length;
+          this.adaptResult(res.data, this.graphData, 1);
+          this.adaptResult(res.data, this.graphDataU, 2);
         }
       },
       err => console.log(err),
       () => this.cd.detectChanges()
     );
+
     this.chartParameters = new DiscreteBarChartConfiguration();
-    this.chartParameters.xAxis = "key";
-    this.chartParameters.yAxis = ["values"];
-    this.chartParameters.color = ['#4649A6', '#ffcc33', '#e84b2c', '#006bdb', '#41bf78'];
+    this.chartParameters.height = 130;
+    this.chartParameters.showLegend = false;
+    this.chartParameters.y1Axis.showMaxMin = false;
+    this.chartParameters.x1Axis.showMaxMin = false;
+    this.chartParameters2 = new DiscreteBarChartConfiguration();
+    this.chartParameters2.height = 500;
+    this.chartParameters2.xAxis = "key";
+    this.chartParameters2.yAxis = ["values"];
+    this.chartParameters2.color = ['#4649A6', '#006bdb', '#363636', '#1464a5','#41bf78'];
+    this.chartParameters2.x1Axis.orient = "bottom";
+    this.chartParameters2.x1Axis.rotateLabels = 270;
   }
-  adaptResult(data: any) {
-    this.graphData = [];
+  adaptResult(data: any, graphData: any[], numero: number) {
     if (data && data.length) {
-      let values = this.processValues(data);
-      let keys = this.processKeys(data);
-      keys.forEach((item: any, items: number) => {
-        const linea: object[] = [{'key': item, 'values': values[items]}]; console.log(linea)
-       this.graphData.push(linea[0]); console.log(this.graphData);
-      });
-      let dataAdapter = DataAdapterUtils.createDataAdapter(this.chartParameters);
-      this.discretebar.setDataArray(dataAdapter.adaptResult(this.graphData));
+      if(numero === 1){
+        let values = this.processValues(data, this.auxGraph);
+        // chart data
+        this.graphData = [
+          {
+            'key': 'Discrete serie',
+            'values': values
+          }
+        ];
+      }else if(numero === 2 && this.auxGraph.length > 0){
+        let values = this.processKeyValues(this.auxGraph);
+        this.graphDataU = [
+          {
+            'key': 'Users',
+            'values': values
+          }
+        ];
+      }
+      
     }
   }
-  processKeys(data: any) {
-    let keys = [];
-    data.forEach((item: any) => {
-      let id_name = 'Ref: ' + item.id_user + ' - ' + item.name + ' ' + item.surname1;
-      keys.push(id_name);
-    });
-    return keys;
-  }
-  processValues(data: any) {
+  processValues(data: any, graphData: any[]) {
     let values = [];
-    data.forEach((item: any) => {
-    values.push(item.not_picked_up); 
+    let minorValue = 0;
+    let majorValue = 0;
+    data.forEach((item: any, index: number) => {
+      if (item['not_picked_up'] >= this.criteriaValue){
+        majorValue++;
+        graphData.push(item);
+      }else{
+        minorValue++;
+      }
+    });
+
+    let lowerCrit = {
+      'x': 'Under',
+      'y': minorValue
+    }
+
+    let upperCrit = {
+      'x': 'Over',
+      'y': majorValue
+    }
+
+    values.push(lowerCrit);
+    values.push(upperCrit);
+    return values;
+  }
+  processKeyValues(graphData: any[]) {
+    let values = []; console.log(graphData)
+    graphData.forEach((item: any, index: number) => {
+      let user = {
+        'x': item.id_user + '; ' + (item.name).charAt(0) + '. ' + item.surname1,
+        'y': item.not_picked_up
+      }
+      values.push(user);
     });
     return values;
-  }  
-  
+  }
   ngOnInit() {
   }
-
 }
+
+
--- Front/src/app/main/main-routing.module.ts
+++ Front/src/app/main/main-routing.module.ts
@@ -48,25 +48,15 @@ export const routes: Routes = [
           }
         } 
       },
-      // { path: 'booking-charts-profits', loadChildren: () => import('./booking-charts-profits/booking-charts-profits.module').then(m => m.BookingChartsProfitsModule), 
-      //   data: {
-      //     oPermission: {
-      //       permissionId: 'booking-charts-sells-permissions',
-      //       restrictedPermissionsRedirect: '/main/home',
-      //       // restrictedPermissionsRedirect: '403',
-      //     }
-      //   } 
-      // },
-      // { path: 'booking-charts-sells', loadChildren: () => import('./booking-charts-sells/booking-charts-sells.module').then(m => m.BookingChartsSellsModule), 
-      //   data: {
-      //     oPermission: {
-      //       permissionId: 'booking-charts-sells-permissions',
-      //       restrictedPermissionsRedirect: '/main/home',
-      //       // restrictedPermissionsRedirect: '403',
-      //     }
-      //   } 
-      // },
-      { path: 'booking-charts', loadChildren: () => import('./booking-charts/booking-charts.module').then(m => m.BookingChartsModule)},
+      { path: 'booking-charts', loadChildren: () => import('./booking-charts/booking-charts.module').then(m => m.BookingChartsModule), 
+        data: {
+          oPermission: {
+            permissionId: 'booking-charts-permissions',
+            restrictedPermissionsRedirect: '/main/home',
+            // restrictedPermissionsRedirect: '403',
+          }
+        } 
+      },
     
     ]
   }
