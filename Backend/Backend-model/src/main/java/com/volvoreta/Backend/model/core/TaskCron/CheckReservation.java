98a022bbf044e6100c003442587331f39dbbed4e
Virginia M García Álvarez <vmgalvz@gmail.com>
2023-07-18 18:07:48 +0200

    corrección check

--- Backend/Backend-model/src/main/java/com/volvoreta/Backend/model/core/TaskCron/CheckReservation.java
+++ Backend/Backend-model/src/main/java/com/volvoreta/Backend/model/core/TaskCron/CheckReservation.java
@@ -1,8 +1,11 @@
 package com.volvoreta.Backend.model.core.TaskCron;
 
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import com.volvoreta.Backend.api.core.service.IBookingService;
 import com.volvoreta.Backend.api.core.service.IProductService;
+import com.volvoreta.Backend.model.core.dao.BookingDao;
+import com.volvoreta.Backend.model.core.dao.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -48,16 +51,20 @@ public class CheckReservation {
         Map<String, Object> filterBooking = new HashMap<>();
         List <String> atributtesBooking = new ArrayList<>(Arrays.asList("id", "id_product", "units", "reservation_state", "end_date"));
 
-        EntityResult listBooking;
-        listBooking = this.bookingService.bookingQuery(filterBooking,atributtesBooking);
+        EntityResult listBooking = this.bookingService.bookingQuery(filterBooking,atributtesBooking);
 
         // Si esta vacio retorna
-        if(listBooking.isEmpty()) return;
+        if(listBooking.isEmpty()){
+            return;
+        }
 
         List <Map<String, Object>> listBookingFormat = new ArrayList<>();
 
         for (int i = 0; i < listBooking.calculateRecordNumber(); i++) {
-            listBookingFormat.add(listBooking.getRecordValues(i));
+            if(listBooking.getRecordValues(i).get("reservation_state").equals(1)){
+                listBookingFormat.add(listBooking.getRecordValues(i));
+            }
+
         }
 
         // Comprobamos las fechas
@@ -68,10 +75,9 @@ public class CheckReservation {
              if(endDate.isBefore(today)){
 
                  Map<String, Object> attrMap = new HashMap<>();
-                 attrMap.put("reservation_state","4");
+                 attrMap.put("reservation_state", 4);
                  Map<String, Object> keyMap = new HashMap<>();
                  keyMap.put("id", booking.get("id"));
-
                  EntityResult updatedBooking = bookingService.bookingUpdate(attrMap, keyMap);
 
                  if(updatedBooking.getCode() != 1){
